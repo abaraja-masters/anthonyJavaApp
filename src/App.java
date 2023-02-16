6ac976f868a162e945170481d58747185b20489d
Anthony Baraja <atbaraja@gmail.com>
2023-02-16 00:38:57 -0800

    Initial Java files

---
+++ src/App.java
@@ -0,0 +1,11 @@
+
+/**
+ * This is my main driver class, I will be developing fundamentals and advance Java concepts.
+ */
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+
+        
+    }
+}
---
+++ src/TestApp.java
@@ -0,0 +1,13 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
+/**
+ * This is my unit testing file, so far it's just hard coded test case, it's just practice.
+ */
+public class TestApp {
+    @Test
+    public void test(){
+        assertEquals(2, 1+1);
+    }
+}
